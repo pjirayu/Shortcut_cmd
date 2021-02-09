7266ac5965e7763c1738aa9fd1808f5be43bca0b
Jirayu Petchhan <75000395+pjirayu@users.noreply.github.com>
2021-02-09 23:36:09 +0800

    Update Torch_cmd.py

--- Torch_cmd.py
+++ Torch_cmd.py
@@ -5,9 +5,11 @@ import torchvision
 # Download resnet18
 model = torchvision.models.resnet18(pretrained=True)
 
+###################################################
 # Freeze all the layers bar the last one
 for param in model.parameters():
     param.requires_grad = False
+###################################################
 
 # Change the number of output units
 model.fc = nn.Linear(512, 7)
