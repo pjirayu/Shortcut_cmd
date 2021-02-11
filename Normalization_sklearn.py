8b2398f344f56378894235afcab3a6a40d031a8a
Jirayu Petchhan <75000395+pjirayu@users.noreply.github.com>
2021-02-11 15:41:23 +0800

    Update Normalization_sklearn.py

--- Normalization_sklearn.py
+++ Normalization_sklearn.py
@@ -47,3 +47,20 @@ print(so_numeric_df[['Age_SS', 'Age']].head())
 4  0.226214   39
 '''
 ########################################
+### Log Transformation
+# Import PowerTransformer
+from sklearn.preprocessing import PowerTransformer
+
+# Instantiate PowerTransformer
+pow_trans = PowerTransformer()
+
+# Train the transform on the data
+pow_trans.fit(so_numeric_df[['ConvertedSalary']])
+
+# Apply the power transform to the data
+so_numeric_df['ConvertedSalary_LG'] = pow_trans.transform(so_numeric_df[['ConvertedSalary']])
+
+# Plot the data before and after the transformation
+so_numeric_df[['ConvertedSalary', 'ConvertedSalary_LG']].hist()
+plt.show()
+########################################
