a1a0f5f95ec53b99bf9d966709046e317babcd35
Jirayu Petchhan <75000395+pjirayu@users.noreply.github.com>
2021-02-11 15:26:01 +0800

    Update Normalization_sklearn.py

--- Normalization_sklearn.py
+++ Normalization_sklearn.py
@@ -1,4 +1,5 @@
 ########################################
+### Normalization
 # Import MinMaxScaler
 from sklearn.preprocessing import MinMaxScaler
 
@@ -13,8 +14,16 @@ so_numeric_df['Age_MM'] = MM_scaler.transform(so_numeric_df[['Age']])
 
 # Compare the origional and transformed column
 print(so_numeric_df[['Age_MM', 'Age']].head())
-
+'''
+     Age_MM  Age
+0  0.046154   21
+1  0.307692   38
+2  0.415385   45
+3  0.430769   46
+4  0.323077   39
+'''
 ########################################
+### Stardardization
 # Import StandardScaler
 from sklearn.preprocessing import StandardScaler
 
@@ -29,4 +38,12 @@ so_numeric_df['Age_SS'] = SS_scaler.transform(so_numeric_df[['Age']])
 
 # Compare the origional and transformed column
 print(so_numeric_df[['Age_SS', 'Age']].head())
+'''
+     Age_SS  Age
+0 -1.132431   21
+1  0.150734   38
+2  0.679096   45
+3  0.754576   46
+4  0.226214   39
+'''
 ########################################
